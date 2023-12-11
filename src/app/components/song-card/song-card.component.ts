4d5afaa778502554447fff3b389ebb53609fef23
alejandro <alejandro.12w@gmail.com>
2023-12-10 23:22:15 -0800

    working on playlists

--- src/app/components/song-card/song-card.component.ts
+++ src/app/components/song-card/song-card.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { SpotifyServiceService } from '../../services/spotify-service.service';
 
 @Component({
   selector: 'app-song-card',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './song-card.component.html',
-  styleUrl: './song-card.component.scss'
+  styleUrl: './song-card.component.scss',
+  providers: [SpotifyServiceService]
 })
 export class SongCardComponent {
+
+  artistName: string = 'playlists';
+  musicData: any;
+
+  constructor(private spotifyService: SpotifyServiceService) { }
+
+  ngOnInit() {
+    this.getPlayList(this.artistName);
+    // this.artistName = '';
+  }
+
+  onSubmit(){
+    // this.getWeatherData(this.cityName);
+    // this.cityName = '';
+  }
+
+  private getPlayList(artistName_: string){
+    const artistName = artistName_; 
+    this.spotifyService.searchPlaylists(artistName).subscribe(
+      (data: any) => {
+        this.musicData = data;
+        console.log(data); 
+      }
+    );
+  }
 
 }
